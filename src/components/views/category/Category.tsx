b34a24a8f5fea94c5f1004b20d250e2011346d10
Mazyaa <muhamadazisudarya@unbaja.ac.id>
2025-09-30 21:06:00 +0700

    feat: have been created template for category page

---
+++ src/components/views/category/Category.tsx
@@ -0,0 +1,9 @@
+const Category = () => {
+    return(
+        <div>
+            <h1>Test Page Category</h1>
+        </div>
+    )
+}
+
+export default Category;
---
+++ src/pages/admin/category/index.tsx
@@ -0,0 +1,16 @@
+import DashboardLayout from "@/components/layouts/DashboardLayout";
+import Category from "@/components/views/category";
+
+const CategoryPage = () => {
+  return (
+    <DashboardLayout
+      title="Category"
+      description="List of all Cartegory, create new categories, and manage existing categoies"
+      type="admin"
+    >
+      <Category />
+    </DashboardLayout>
+  );
+};
+
+export default CategoryPage;
