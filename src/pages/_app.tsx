c03f0ba3062feb18bf7724af44d9242da81a6363
Mazyaa <muhamadazisudarya@unbaja.ac.id>
2025-10-23 20:54:51 +0700

    docs: add some docs for main file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,23 +6,22 @@ import { SessionProvider } from "next-auth/react";
 import AppShell from "@/components/commons/AppShell";
 
 
-
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false,
-      retry: false,
+      refetchOnWindowFocus: false, // if change tab, do not refetch
+      retry: false, // if fail, do not retry refetch
     },
   },
 });
 
 export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
+  Component, // page which is being rendered
+  pageProps: { session, ...pageProps }, // props of the page
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <QueryClientProvider client={queryClient}>
+    <SessionProvider session={session}> // provide session to the app
+      <QueryClientProvider client={queryClient}> // provide react-query client to the app
         <HeroUIProvider>
          <AppShell>
             <Component {...pageProps} />
