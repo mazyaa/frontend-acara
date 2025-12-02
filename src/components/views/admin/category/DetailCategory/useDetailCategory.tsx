0a2e03a806f9127dcfef071c6edf69cbb7ca8d0f
Mazyaa <muhamadazisudarya@unbaja.ac.id>
2025-12-02 22:33:14 +0700

    feat: create fn for getCategoryById use useQuery

--- src/components/views/admin/category/DetailCategory/useDetailCategory.tsx
+++ src/components/views/admin/category/DetailCategory/useDetailCategory.tsx
@@ -1,4 +1,5 @@
 import categoryServices from "@/services/category.service";
+import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
 
 const useDetailCategory = () => {
@@ -8,5 +9,15 @@ const useDetailCategory = () => {
         const {data} = await categoryServices.getCategoryById(id);
         
         return data.data;
+    };
+
+    const {data: dataCategory} = useQuery({ // use useQuery to fetch category details
+        queryKey: ['category'], // unique key for the query
+        queryFn: () => getCategoryById(`${query.id}`), // fetch category by id from the query parameters
+        enabled: isReady, // ensure the query runs only when the router is ready
+    });
+
+    return {
+        dataCategory,
     };
  }
